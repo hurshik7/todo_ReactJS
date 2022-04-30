9e58c9f1c6bfae16adf86acdf3a586583a62c889
hurshik7 <hurshik7@gmail.com>
2022-04-30 16:18:57 -0700

    add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class Myheader extends Component {
+  render() {
+    return (
+      <header>
+        <h1 className='logo'>{this.props.title}</h1>
+        <p>{this.props.desc}</p>
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+class Mynav extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="">HTML</a></li>
+          <li><a href="">CSS</a></li>
+          <li><a href="">Javascript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Myarticle extends Component {
+  render() {
+    return (
+      <section>
+        <article>
+          <h2>HTML</h2>
+          <p>Hypertext Markup Language</p>
+        </article>
+      </section>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Myheader title="React" desc="Todo list"></Myheader>
+        <Mynav></Mynav>
+        <Myarticle></Myarticle>
+      </div>
+    )
+  }
 }
 
 export default App;
