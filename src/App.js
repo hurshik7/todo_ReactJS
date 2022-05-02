223942e96b9d42cad192c178f1e9b1a0263c81d8
hurshik7 <hurshik7@gmail.com>
2022-05-01 22:21:33 -0700

    add data

--- src/App.js
+++ src/App.js
@@ -5,12 +5,33 @@ import Mynav from "./Mynav";
 import Myarticle from "./Myarticle";
 
 class App extends Component {
+  // 여기에 constructor 써야함
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: "React", desc: "Todo list" },
+      menus: [
+        {id: 1, title: 'HTML', desc: 'Hypertext Markup Language'},
+        {id: 2, title: 'CSS', desc: 'CSS is for design'},
+        {id: 3, title: 'Javascript', desc: 'Javascript is for interactive'}
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <Myheader title="React" desc="Todo list"></Myheader>
-        <Mynav></Mynav>
-        <Myarticle title="HTML" desc="Hypertext Markup Language"></Myarticle>
+        <Myheader
+          title={this.state.subject.title}
+          desc={this.state.subject.desc}
+        ></Myheader>
+        <Mynav 
+          data={this.state.menus}>
+        </Mynav>
+        <Myarticle 
+          title="HTML" 
+          desc="Hypertext Markup Language">
+        </Myarticle>
       </div>
     );
   }
