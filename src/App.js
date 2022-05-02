c0d3c2c4afec8f30ec0e10378a0da8f8bad15e6f
hurshik7 <hurshik7@gmail.com>
2022-05-01 20:04:19 -0700

    detach components

--- src/App.js
+++ src/App.js
@@ -1,53 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
-
-class Myheader extends Component {
-  render() {
-    return (
-      <header>
-        <h1 className='logo'>{this.props.title}</h1>
-        <p>{this.props.desc}</p>
-      </header>
-    );
-  }
-}
-
-class Mynav extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li><a href="">HTML</a></li>
-          <li><a href="">CSS</a></li>
-          <li><a href="">Javascript</a></li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Myarticle extends Component {
-  render() {
-    return (
-      <section>
-        <article>
-          <h2>HTML</h2>
-          <p>Hypertext Markup Language</p>
-        </article>
-      </section>
-    )
-  }
-}
+import React, { Component } from "react";
+import "./App.css";
+import Myheader from "./Myheader";
+import Mynav from "./Mynav";
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <Myheader title="React" desc="Todo list"></Myheader>
         <Mynav></Mynav>
-        <Myarticle></Myarticle>
+        <Myarticle title="HTML" desc="Hypertext Markup Language"></Myarticle>
       </div>
-    )
+    );
   }
 }
 
---
+++ src/Mynav.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export default class Mynav extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="">HTML</a>
+          </li>
+          <li>
+            <a href="">CSS</a>
+          </li>
+          <li>
+            <a href="">Javascript</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
